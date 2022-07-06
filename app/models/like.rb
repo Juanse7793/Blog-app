6962244fa3e6dfbfe836473739121644abb60055
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-06 16:23:13 -0500

    after save & private

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :like_count
+
+  private
+
   def like_count
     post.update(likes_counter: post.likes.count)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :post_count
+
   def five_last_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  private
 
   def post_count
     author.update(posts_counter: author.posts.count)
