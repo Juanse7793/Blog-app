d9db3d754a2c86673356754001134ab195468325
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-06 15:19:43 -0500

    post count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,4 @@ class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :author , class_name: "User"
 
-    def update_comments_count
-        
-    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
     def five_last_comments
         comments.order(created_at: :desc).limit(5)
     end
+
+    def post_count
+        author.update(posts_counter: author.posts.count)
+    end
 end
