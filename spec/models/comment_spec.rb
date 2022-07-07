ca6d48a26cf9597a470f4ad39902665cc634ccb2
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-07 11:36:43 -0500

    comment tests added

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+    before :each do
+      @user = User.new(name: 'John Doe', photo: 'https://example.com/photo.jpg', bio: 'hola', posts_counter: 0)
+      @user.save
+      @post = Post.new(author: @user, title: 'Post title', text: 'body', comments_counter: 0, likes_counter: 0)
+      @post.save
+      @comment = Comment.new(author: @user, text: 'comment', post: @post)
+    end
+
+    it 'text always valid' do
+        @comment.text = 'comment'
+        expect(@comment).to be_valid
+    end
+
+    it 'text always valid' do
+        @comment.text = nil
+        expect(@comment).to be_valid
+    end
+end
