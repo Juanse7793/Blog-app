05da5e7efd0534f6085d928c3979167f1aca3ab1
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-07 10:24:46 -0500

    post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,7 @@ class Post < ApplicationRecord
   def post_count
     author.update(posts_counter: author.posts.count)
   end
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
