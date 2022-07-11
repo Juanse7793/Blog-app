28fb45432714dc691cce00a1b9cdfb86bc45ce96
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-11 18:49:29 -0500

    linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
 
   after_initialize :post_count_value
 
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
