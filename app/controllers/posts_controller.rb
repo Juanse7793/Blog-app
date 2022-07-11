e3e260f8ded3c9e7ded84517b143386face9ff1a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-11 11:33:29 -0500

    linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-    def index; end
+  def index; end
 
-    def show
-        puts params
-    end
+  def show
+    puts params
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    def index; end
+  def index; end
 
-    def show
-        puts params
-    end
+  def show
+    puts params
+  end
 end
