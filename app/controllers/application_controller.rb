a58e8d6f43a2ec84341bfc886cb15e7629ad47dd
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-12 11:30:49 -0500

    create user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def create_user
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "User created successfully"
+      redirect_to users_path
+    else
+      render :action => 'new'
+    end
+  end
+
 end
