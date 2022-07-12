f1bdc78e0c50f726bc1ea5eda4cc39d8a4378057
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-12 14:55:39 -0500

    form adding post to db

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 root 'users#index'
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
+  resources :posts, only: [:new, :create]
 end
