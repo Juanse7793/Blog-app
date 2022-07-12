cd425dbe9ed799243e35d2fb8cc02af2fe08bdc9
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-12 16:24:33 -0500

    linters

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'stimulus-rails'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem 'jbuilder'
 
-gem "font-awesome-sass", "~> 6.1.1"
+gem 'font-awesome-sass', '~> 6.1.1'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-
-#   add a method to return the first user in the database
-    def current_user
-      @current_user = User.first
-    end
+  #   add a method to return the first user in the database
+  def current_user
+    @current_user = User.first
+  end
 end
