f209a01635eb7b084eaa96b7632bbe7aba4349d3
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-12 16:52:52 -0500

    linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
-    def like
-        @post = Post.find(params[:id])
-        Like.new(author: current_user, post: @post).save
-        redirect_to user_post_path(current_user, @post)
-      end
+  def like
+    @post = Post.find(params[:id])
+    Like.new(author: current_user, post: @post).save
+    redirect_to user_post_path(current_user, @post)
+  end
 end
