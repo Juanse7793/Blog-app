2e46663e7038e6a27e09ee06e3c7eef7762e941b
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-13 15:39:44 -0500

    is liked

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,17 @@
 class LikesController < ApplicationController
   def like
+    @user = current_user
     @post = Post.find(params[:id])
-    Like.new(author: current_user, post: @post).save
-    redirect_to user_post_path(current_user, @post)
+    if already_liked?
+      redirect_to user_post_path(current_user, @post)
+    else
+      @like = Like.new(author: @user, post: @post)
+      @like.save
+      redirect_to user_post_path(current_user, @post)
+    end
+  end
+
+  def already_liked?
+    Like.where(author: current_user, post: @post).exists?
   end
 end
