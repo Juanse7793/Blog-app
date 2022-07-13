6b88ad13124f4761d077f17cb7dc2a9d61d8d6ac
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-13 16:04:17 -0500

    linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
     @user = current_user
     @post = Post.find(params[:id])
     if already_liked?
-      redirect_to user_post_path(current_user, @post)
+      flash[:notice] = "You can't like more than once"
     else
       @like = Like.new(author: @user, post: @post)
       @like.save
-      redirect_to user_post_path(current_user, @post)
     end
+    redirect_to user_post_path(current_user, @post)
   end
 
   def already_liked?
