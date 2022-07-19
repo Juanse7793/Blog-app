60b36cc8ec62b1b760f0f3330c47ba7465360184
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-19 15:40:11 -0500

    linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.includes(comments:[:author]).find(params[:id])
+    @post = Post.includes(comments: [:author]).find(params[:id])
     @user = User.find(params[:user_id])
   end
 
