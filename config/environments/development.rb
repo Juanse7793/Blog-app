add6fc1fa4900049ded1561d27db300e70bfdcee
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-19 15:39:17 -0500

    fix errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,14 +49,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: 'b62fd07b9f9bde',
     password: '1b99ee2bc8b135',
     domain: 'mailtrap.io',
     address: 'smtp.mailtrap.io',
-    port: '25',
+    port: '465',
     authentication: :plain,
     enable_starttls_auto: true
   } 
