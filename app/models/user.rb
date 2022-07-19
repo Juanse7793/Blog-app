d21ca1b228f1c8d15971125db9301c8ce598638e
Henry Nawrocki <hdev@sztanski.com>
2022-07-19 12:26:19 -0500

    Signup and Login work

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,16 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 
-  after_initialize :post_count_value
+  after_initialize :post_count_value, :default_photo
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def post_count_value
     self.posts_counter ||= 0
+  end
+
+  def default_photo
+    self.photo ||= 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
   end
 end
